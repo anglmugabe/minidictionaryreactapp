6340080e3b8963790895bed6c4391b5a76fbd63a
anglmugabe <anglmugabe@gmail.com>
2025-01-11 16:00:07 +0200

    added images to dictionary component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,33 +6,63 @@ import "./Dictionary.css";
 export default function Dictionary() {
 	let [keyword, setKeyword] = useState(" ");
 	let [results, setResults] = useState(null);
+	let [loaded, setLoaded] = useState(false);
+	let [photos, setPhotos] = useState(null);
 
 	function handleResponse(response) {
 		setResults(response.data[0]);
 	}
 
-	function search(event) {
-		event.preventDefault();
+	function handleImagesResponse(response) {
+		setPhotos(response.data.photos);
+	}
 
-		let apiKey = "cadc0335bc0tc128c364a2674f06oeee1";
+	function search() {
+		const apiKey = "cadc0335bc0tc128c364a2674f06oeee1";
+
 		let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 		axios.get(apiUrl).then(handleResponse);
+
+		let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+		axios.get(imagesApiUrl, { headers: headers }).then(handleImagesResponse);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
 	}
 
 	function handleKeywordChange(event) {
 		setKeyword(event.target.value);
 	}
 
-	return (
-		<div className="Dictionary">
-			<form onSubmit={search}>
-				<input
-					type="search"
-					onChange={handleKeywordChange}
-					placeholder="Let's find your word..."
-				/>
-			</form>
-			<Results />
-		</div>
-	);
+	function load() {
+		setLoaded(true);
+		search();
+	}
+
+	if (loaded) {
+		return (
+			<div className="Dictionary">
+				<section>
+					<form onSubmit={search}>
+						<input
+							type="search"
+							onChange={handleKeywordChange}
+							defaultValue={props.defaultKeyword}
+							placeholder="Let's find your word..."
+						/>
+					</form>
+					<div className="hint">
+						suggested words: summer, sugar, book, love...
+					</div>
+				</section>
+				<Results results={results} />
+				<Photos photos={photos} />
+			</div>
+		);
+	} else {
+		load();
+		return "Loading";
+	}
 }
